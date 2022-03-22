021dd7c6942945be5a460c7235fff99ecbc9c854
Rahul Salunkhe <rahul.salunkhe@publicissapient.com>
2022-03-22 18:08:28 +0530

    Add secondary navigation.

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ContainerStyles from "../styles/styles/container.styles";
 import CartIconComponent from "../cart/cart-icon/cart.icon.components";
-import { Link } from "react-router-dom";
 import {
   HeaderMain,
   HeaderContainer,
+  LogoLink,
   Logo,
   LinkCon,
   NavLink,
+  HeaderLeft,
+  SecLinkCon,
+  SecLink,
 } from "./header.styles";
 
 const Header = (props) => {
@@ -15,14 +18,20 @@ const Header = (props) => {
     <HeaderMain>
       <ContainerStyles>
         <HeaderContainer>
-          <Link to="/">
+          <LogoLink to="/">
             <Logo src="/static/images/logo.png" alt="Logo" />
-          </Link>
+          </LogoLink>
           <LinkCon>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/products">Products</NavLink>
           </LinkCon>
-          <CartIconComponent />
+          <HeaderLeft>
+            <SecLinkCon>
+              <SecLink to="/signin">SignIn</SecLink>
+              <SecLink to="/register">Register</SecLink>
+            </SecLinkCon>
+            <CartIconComponent />
+          </HeaderLeft>
         </HeaderContainer>
       </ContainerStyles>
     </HeaderMain>
--- client/src/components/header/header.styles.js
+++ client/src/components/header/header.styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { devices } from "../../styles/breakpoints";
 
 export const HeaderMain = styled.header`
   box-shadow: 0 1.5rem 1rem rgba(155, 155, 155, 0.1);
@@ -10,8 +11,15 @@ export const HeaderContainer = styled.div`
   display: flex;
 `;
 
+export const LogoLink = styled(Link)`
+  align-self: center;
+`;
+
 export const Logo = styled.img`
   /* width: 38rem; */
+  @media ${devices.mobile} {
+    width: 15rem;
+  }
 `;
 
 export const LinkCon = styled.nav`
@@ -36,4 +44,22 @@ export const CartIcon = styled.div`
   font-size: 1.8rem;
   align-self: stretch;
   color: #fff;
+  align-self: flex-end;
+`;
+
+export const HeaderLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const SecLinkCon = styled.nav`
+  display: flex;
+  margin-top: 1rem;
+`;
+
+export const SecLink = styled(Link)`
+  font-size: 1.6rem;
+  color: #000;
+  margin-left: 2rem;
 `;
